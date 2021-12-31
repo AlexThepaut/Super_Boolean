3db2ecc2dc29949d6270414ac328bf0936cd10b2
Alex <alex.thepaut33@gmail.com>
2021-12-31 14:30:40 +0100

    Complete Readme and add truth table generator

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Super_Boolean
 Module for dealing with MOTHERFUCKING boolean
+
+## NOT
+| True     | False   | MF True | MF False |
+|----------|---------|---------|----------|
+| MF False | MF True | True    | False    |
+
+## OR
+|          | True    | False    | MF True | MF False |
+|----------|---------|----------|---------|----------|
+| True     | True    | True     | MF True | False    |
+| False    | True    | False    | True    | MF False |
+| MF True  | MF True | True     | MF True | null     |
+| MF False | False   | MF False | null    | MF False |
+
+## AND
+|          | True  | False | MF True | MF False |
+|----------|-------|-------|---------|----------|
+| True     | True  | False | True    | False    |
+| False    | False | False | True    | False    |
+| MF True  | True  | True  | MF True | null     |
+| MF False | False | False | null    | MF False |
+
+## XOR
+|          | True     | False | MF True | MF False |
+|----------|----------|-------|---------|----------|
+| True     | True     | False | null    | MF False |
+| False    | False    | False | True    | null     |
+| MF True  | null     | True  | True    | True     |
+| MF False | MF False | null  | True    | MF False |
--- lib/SuperBoolean.ts
+++ lib/SuperBoolean.ts
@@ -5,29 +5,36 @@ export enum SuperBoolean {
     MOTHERFUCKING_FALSE
 }
 
+export enum SuperOperator {
+    SUPER_NOT,
+    SUPER_OR,
+    SUPER_AND,
+    SUPER_XOR
+}
+
 export class SuperBooleanOperator {
     private static MASQUARADES: Array<Masquarade> = [
-        {'id': SuperBoolean.TRUE, 'state': true, 'motherfucking': false},
-        {'id': SuperBoolean.FALSE, 'state': false, 'motherfucking': false},
-        {'id': SuperBoolean.MOTHERFUCKING_TRUE, 'state': true, 'motherfucking': true},
-        {'id': SuperBoolean.MOTHERFUCKING_FALSE, 'state': false, 'motherfucking': true}
+        { 'id': SuperBoolean.TRUE, 'state': true, 'motherfucking': false },
+        { 'id': SuperBoolean.FALSE, 'state': false, 'motherfucking': false },
+        { 'id': SuperBoolean.MOTHERFUCKING_TRUE, 'state': true, 'motherfucking': true },
+        { 'id': SuperBoolean.MOTHERFUCKING_FALSE, 'state': false, 'motherfucking': true }
     ];
 
     public static SUPER_NOT(sp: SuperBoolean): SuperBoolean | null {
         const temp = this.findTheMasquarade(sp);
-        return this.findSuperBoolean(!temp.state, !temp.motherfucking);
+        return this.findSuperBoolean(temp.motherfucking ? temp.state : !temp.state, !temp.motherfucking);
     }
 
     public static SUPER_OR(sp1: SuperBoolean | null, sp2: SuperBoolean | null): SuperBoolean | null {
-        if (sp1 && sp2) {
+        if (sp1 !== null && sp2 !== null) {
             const tempSp1 = this.findTheMasquarade(sp1);
             const tempSp2 = this.findTheMasquarade(sp2);
-    
+
             let resultState = false;
             let resultMf = false;
-            if(!tempSp1.motherfucking && !tempSp2.motherfucking) {
+            if (!tempSp1.motherfucking && !tempSp2.motherfucking) {
                 resultState = tempSp1.state || tempSp2.state;
-            } else if (tempSp1.motherfucking && tempSp2.motherfucking){
+            } else if (tempSp1.motherfucking && tempSp2.motherfucking) {
                 if (tempSp1.state !== tempSp2.state) return null;
                 resultMf = true;
                 resultState = tempSp1.state;
@@ -35,23 +42,23 @@ export class SuperBooleanOperator {
                 resultMf = tempSp1.state === tempSp2.state;
                 resultState = tempSp1.motherfucking ? tempSp1.state : tempSp2.state;
             }
-    
+
             return this.findSuperBoolean(resultState, resultMf);
         }
         return null;
     }
-    
+
     public static SUPER_AND(sp1: SuperBoolean | null, sp2: SuperBoolean | null): SuperBoolean | null {
-        if (sp1 && sp2) {
+        if (sp1 !== null && sp2 !== null) {
             const tempSp1 = this.findTheMasquarade(sp1);
             const tempSp2 = this.findTheMasquarade(sp2);
-    
+
             let resultState = false;
             let resultMf = false;
-    
-            if(!tempSp1.motherfucking && !tempSp2.motherfucking) {
+
+            if (!tempSp1.motherfucking && !tempSp2.motherfucking) {
                 resultState = tempSp1.state && tempSp2.state;
-            } else if (tempSp1.motherfucking && tempSp2.motherfucking){
+            } else if (tempSp1.motherfucking && tempSp2.motherfucking) {
                 if (tempSp1 !== tempSp2) return null;
                 resultMf = true;
                 resultState = tempSp1.state;
@@ -59,7 +66,7 @@ export class SuperBooleanOperator {
                 resultMf = false;
                 resultState = tempSp1.motherfucking ? tempSp1.state : tempSp2.state;
             }
-    
+
             return this.findSuperBoolean(resultState, resultMf);
         }
         return null;
@@ -69,6 +76,51 @@ export class SuperBooleanOperator {
         return this.SUPER_OR(this.SUPER_AND(sp1, this.SUPER_NOT(sp2)), this.SUPER_AND(this.SUPER_NOT(sp1), sp2))
     }
 
+    public static SUPER_OPERATOR_TRUTH_TABLE(so: SuperOperator): Array<Array<SuperBoolean | null>> | null {
+        let truthTable: Array<Array<SuperBoolean | null>> = [];
+
+        switch (so) {
+            case SuperOperator.SUPER_NOT:
+                const tempRow: Array<SuperBoolean | null> = [];
+                for (const sb of this.MASQUARADES) {
+                    tempRow.push(this.SUPER_NOT(sb.id));
+                }
+                truthTable.push(tempRow);
+                break;
+            case SuperOperator.SUPER_OR:
+                for (const sb1 of this.MASQUARADES) {
+                    const tempRow: Array<SuperBoolean | null> = [];
+                    for (const sb2 of this.MASQUARADES) {
+                        tempRow.push(this.SUPER_OR(sb1.id, sb2.id));
+                    }
+                    truthTable.push(tempRow);
+                }
+                break;
+            case SuperOperator.SUPER_AND:
+                for (const sb1 of this.MASQUARADES) {
+                    const tempRow: Array<SuperBoolean | null> = [];
+                    for (const sb2 of this.MASQUARADES) {
+                        tempRow.push(this.SUPER_AND(sb1.id, sb2.id));
+                    }
+                    truthTable.push(tempRow);
+                }
+                break;
+            case SuperOperator.SUPER_XOR:
+                for (const sb1 of this.MASQUARADES) {
+                    const tempRow: Array<SuperBoolean | null> = [];
+                    for (const sb2 of this.MASQUARADES) {
+                        tempRow.push(this.SUPER_XOR(sb1.id, sb2.id));
+                    }
+                    truthTable.push(tempRow);
+                }
+                break;
+            default:
+                return null;
+        }
+
+        return truthTable;
+    }
+
     public static findSuperBoolean(state: boolean, isMotherFucking: boolean): SuperBoolean | null {
         const temp = this.MASQUARADES.find(a => a.state === state && a.motherfucking === isMotherFucking);
         if (temp) return temp.id;
@@ -76,9 +128,9 @@ export class SuperBooleanOperator {
     }
 
     private static findTheMasquarade(sp: SuperBoolean): Masquarade {
-            const temp = this.MASQUARADES.find(a => a.id === sp);
-            if (temp) return temp;
-            return this.MASQUARADES[1];
+        const temp = this.MASQUARADES.find(a => a.id === sp);
+        if (temp) return temp;
+        return this.MASQUARADES[1];
     }
 }
 
